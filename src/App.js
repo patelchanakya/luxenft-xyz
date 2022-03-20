1e19b7b088aba849488c542ba5d23814239b11a9
Chanakya <chanakya1patel@gmail.com>
2022-03-19 23:56:13 -0400

    initial
    luxe nft

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h3>LUXE</h3>
+      <div className="container">
+        <h3>LUXE</h3>
+        <br />
+        <h6>nft</h6>
+      </div>
     </div>
   );
 }
